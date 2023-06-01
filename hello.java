1a64fd3e8df887805204c64bdf97881099ef9e5d
bhattnileshr <bhattnileshr@gmail.com>
2023-06-01 20:25:39 +0930

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class Hello {
-  public static void main(Strings[] args) {
-    system.out.println("Hello everyone");
+  public static void main(String[] args) {
+    System.out.println("Hello everyone");
   }
 }
     
